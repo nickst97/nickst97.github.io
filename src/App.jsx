fc351f7460c23cd9a651945ab9fd6ab5caa91416
Nick Stavrakakis <nickst97@outlook.com>
2023-11-18 18:43:17 +0200

    Started navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
 
 function App() {
 	return (
 		<>
 			<Header />
+			<NavBar />
 			<Footer />
 		</>
 	);
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import "../css/NavBar.css";
+export default function NavBar() {
+	return (
+		<nav>
+			<button>
+				<span>Home</span>
+			</button>
+			<button>
+				<span>Projects</span>
+			</button>
+			<button>
+				<span>Awards & Certifications</span>
+			</button>
+			<button>
+				<span>Contact</span>
+			</button>
+		</nav>
+	);
+}
