2722e5fa43a135798e903b3e252167bcca9204e8
Nick Stavrakakis <nickst97@outlook.com>
2023-11-25 18:15:24 +0200

    Finished contact form

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -2,18 +2,23 @@ import { useState } from "react";
 import "../../css/Contact.css";
 
 export default function Contact() {
-	const [numOfNewLines, setNumOfNewLines] = useState(0);
+	// const [numOfNewLines, setNumOfNewLines] = useState(0);
 
-	const handleInput = (e) => {
-		const element = document.getElementById("contact-form-field-message");
-		let newLines = element.getElementsByTagName("div").length;
+	// const handleInput = (e) => {
+	// 	const element = document.getElementById("contact-form-field-message");
 
-		setNumOfNewLines(newLines);
-	};
+	// 	let newLines = element.getElementsByTagName("div").length;
+	// 	console.log(element);
+	// 	if (element.offsetWidth >= 650) {
+	// 		console.log("hey");
+	// 		newLines += 1;
+	// 	}
+	// 	setNumOfNewLines(newLines);
+	// };
 
-	const calculateHeight = () => {
-		return (numOfNewLines + 1) * 22 + "px";
-	};
+	// const calculateHeight = () => {
+	// 	return (numOfNewLines + 1) * 22 + "px";
+	// };
 
 	return (
 		<section id="section-contact">
@@ -23,18 +28,14 @@ export default function Contact() {
 					<div
 						className="section-item-description contact-form-field"
 						contentEditable
-					>
-						Jon Smith
-					</div>
+					></div>
 				</div>
 				<div className="section-item" id="contact-form-email">
 					<div className="section-item-title">Email</div>
 					<div
 						className="section-item-description contact-form-field"
 						contentEditable
-					>
-						email
-					</div>
+					></div>
 				</div>
 				<div className="section-item" id="contact-form-message">
 					<div className="section-item-title">Message</div>
@@ -42,12 +43,11 @@ export default function Contact() {
 						className="section-item-description contact-form-field"
 						contentEditable
 						id="contact-form-field-message"
-						onInput={handleInput}
-						style={{ height: calculateHeight() }}
-					>
-						Hey you beatiful bastard
-					</div>
+					></div>
 				</div>
+				<button className="nav-item">
+					<span>Send</span>
+				</button>
 			</div>
 		</section>
 	);
