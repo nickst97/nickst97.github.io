94d4f72524fbd2d15861da51ec0dfeda1a3dbf8e
Nick Stavrakakis <nickst97@outlook.com>
2023-11-27 00:42:23 +0200

    Progress on wave animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,23 @@
 import "../css/Footer.css";
 export default function Footer() {
-	return <footer></footer>;
+	const waveColor = "var(--black-color)";
+
+	return (
+		<footer>
+			<div id="footer-mask">
+				<div id="footer-mask-left"></div>
+				<div id="footer-mask-center">
+					<div id="footer-mask-center-top"></div>
+					<div id="footer-mask-center-bottom"></div>
+				</div>
+				<div id="footer-mask-right"></div>
+			</div>
+			<div className="ocean" style={{ backgroundColor: waveColor }}>
+				<div
+					className="wave"
+					style={{ backgroundColor: waveColor }}
+				></div>
+			</div>
+		</footer>
+	);
 }
