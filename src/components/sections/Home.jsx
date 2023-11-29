2ec15a24cb653db04694948c97c0e2f530849baa
Nick Stavrakakis <nickst97@outlook.com>
2023-11-30 00:07:45 +0200

    Updated cursor styles

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -6,11 +6,13 @@ export default function Home({ setWaveColor }) {
 			{/* TODO: add the same text to cv */}
 			{/* Revisit how it looks regarding line breaks */}
 			<div className="section-main-content">
-				Full-stack developer with a strong proficiency in developing
-				interactive and dynamic websites. Experienced in team management
-				and project planning, I am passionate about turning ideas into
-				digital experiences with a focus on both creativity and
-				strategic execution.
+				<div className="section-item">
+					Full-stack developer with a strong proficiency in developing
+					interactive and dynamic websites. Experienced in team
+					management and project planning, I am passionate about
+					turning ideas into digital experiences with a focus on both
+					creativity and strategic execution.
+				</div>
 			</div>
 		</section>
 	);
