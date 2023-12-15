18f795424a963ea6a557783ea8a643fc3e94b5f6
Nick Stavrakakis <nickst97@outlook.com>
2023-12-16 00:04:26 +0200

    Trying to fix LCP issue

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import WebFont from "webfontloader";
+WebFont.load({
+	custom: {
+		families: ["Mabry"],
+	},
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
