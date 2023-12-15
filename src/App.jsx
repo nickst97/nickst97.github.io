16635ab6dc8ce966103a561bed7e42095d1dbd26
Nick Stavrakakis <nickst97@outlook.com>
2023-12-15 23:53:46 +0200

    Trying to fix LCP issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import "./index.css";
+import "./fonts.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import SectionContent from "./components/SectionContent";
+
+var WebFont = require("webfontloader");
+WebFont.load({
+	custom: {
+		families: ["Mabry"],
+		urls: ["/fonts.css"],
+	},
+});
 
 const sectionNames = ["Home", "Projects", "Awards & Certifications", "Contact"];
 
