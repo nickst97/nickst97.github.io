61abdf79b02b071c047e54c00a9566f20d04f46a
Nick Stavrakakis <nickst97@outlook.com>
2023-12-27 13:41:00 +0200

    Redirecting subpages to root page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import App from "./components/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Router>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/*" element={<Navigate to="/" />} />
+			</Routes>
+		</Router>
 	</React.StrictMode>
 );
