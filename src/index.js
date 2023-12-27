ceb8172adc97b878b219669687b387f17745ff09
Nick Stavrakakis <nickst97@outlook.com>
2023-12-27 13:47:24 +0200

    Removed "react-router-dom"

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import App from "./components/App";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-	BrowserRouter as Router,
-	Routes,
-	Route,
-	Navigate,
-} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Router>
-			<Routes>
-				<Route path="/" element={<App />} />
-				<Route path="/*" element={<Navigate to="/" />} />
-			</Routes>
-		</Router>
+		<App />
 	</React.StrictMode>
 );
