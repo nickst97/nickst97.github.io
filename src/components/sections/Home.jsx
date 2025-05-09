0fae6b471ea466cc22d43c51765392a45f579a18
Nick Stavrakakis <nickst97@outlook.com>
2025-05-09 14:14:57 +0300

    Updating bio

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,10 +5,9 @@ export default function Home() {
 		<section id="section-home">
 			<div className="section-main-content">
 				<div className="section-item">
-					Frontend developer with a passion for developing interactive
-					and dynamic websites. With experience in team management and
-					project planning, I am passionate about turning ideas into
-					digital experiences with a focus on both creativity and
+					Frontend Developer with experience in delivering
+					high-performing and interactive web applications, turning
+					ideas into digital experiences by combining creativity with
 					strategic execution.
 				</div>
 			</div>
